8032ae8f0af5312a31923ee4efbb302d3a98c0f6
joao victor <victor.jvsb18@gmail.com>
2023-01-10 19:49:55 -0300

    upload files

---
+++ README.md
@@ -0,0 +1,29 @@
+# Edição-de-fotos-em-massa
+Realizar edição de fotos em massa através do script em NodeJS
+
+Adicione sua logo ou imagem em varios fundos de uma vez só com NodeJS!
+
+Instalação:
+
+
+- Instale o FS
+```
+ npm i fs
+```
+
+- Instale o Sharp para edição das fotos
+```
+npm i sharp
+```
+
+Coloque sua logo ou imagem que irá ficar por cima na pasta (images)
+
+Coloque a foto de fundo na pasta principal com nome: fundo.png
+
+- Rode a aplicação
+
+```
+node .
+```
+
+As fotos prontas apareceram na pasta (pront)
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+// Lê a pasta "images"
+fs.readdir('images', (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Para cada arquivo na pasta "images"
+  files.forEach(file => {
+    // Lê a imagem da pasta "images"
+    sharp('images/' + file)
+      // Ajusta o tamanho da imagem para 1900x1080 pixels
+      .resize(1900, 1080, {
+        fit: sharp.fit.contain,
+        background: { r: 255, g: 255, b: 255, alpha: 1 }
+      })
+      // Coloca a imagem da pasta "bigskins" na frente da imagem "fundo.png"
+      .composite([{ input: 'fundo.png', gravity: 'center' }, { input: 'images/' + file, gravity: 'center' }])
+      // Salva a imagem editada na pasta "pront" com o mesmo nome do arquivo original
+      .toFile('pront/' + file, (err, info) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Arquivo ${file} salvo com sucesso!`);
+        }
+      });
+  });
+});
